976bd02dfba21ad6f243e15f75a4468d101e902d
Santiago Ramos <santiagodotsh@gmail.com>
2024-12-08 09:02:39 -0500

    add database sql queries

---
+++ 3_database.sql
@@ -0,0 +1,21 @@
+--------------------------------------------
+-- CREATE DATABASE (creates a database)
+--------------------------------------------
+
+/*
+  CREATE DATABASE is used to create a database.
+  The database name is specified after the CREATE DATABASE keywords.
+*/
+
+CREATE DATABASE mydatabase; -- Creates a database named mydatabase
+
+--------------------------------------------
+-- DROP DATABASE (deletes a database)
+--------------------------------------------
+
+/*
+  DROP DATABASE is used to delete a database.
+  The database name is specified after the DROP DATABASE keywords.
+*/
+
+DROP DATABASE mydatabase; -- Deletes a database named mydatabase
